086f8c12c0b501ab847ea45ce37e76d9bc129370
happenzZ <tsanghaipeng@gmail.com>
2017-07-14 21:16:41 +0800

    add url

--- app/routes/index.py
+++ app/routes/index.py
@@ -11,14 +11,21 @@ def index():
     return json.dumps('Hello world!')
 
 #
-# @app.route('/cropOneImage')
-# def _cropOneImage():
-#     return json.dumps('test')
-#
-#
-# @app.route('/cropOneFolder')
-# def _cropOneFolder():
-#     return json.dumps('test')
+@app.route('/cropOneImage', methods=['POST'])
+def _cropOneImage():
+    post_data = json.loads(request.data.decode())
+    input_path = post_data['inputPath']
+    output_path = post_data['outputPath']
+    result = faceDetection.cut_face_for_image(input_path, output_path)
+    return json.dumps(result)
+
+@app.route('/cropOneFolder', methods=['POST'])
+def _cropOneFolder():
+    post_data = json.loads(request.data.decode())
+    input_folder = post_data['inputFolder']
+    output_folder = post_data['outputFolder']
+    result = faceDetection.cut_face_for_folder(input_folder, output_folder)
+    return json.dumps(result)
 
 #
 # @app.route('/api/update/video/<videoId>', methods=['GET'])
